9f87f3855b6214a3f7d47e5d430db4314a4f1ceb
lesech35230 <denis.lesech@gmail.com>
2018-01-29 14:52:29 +0100

    append module material

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,7 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule  } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { AdhesionComponent } from './adhesion/adhesion.component';
 import { EngagementComponent } from './engagement/engagement.component';
 import { CompetitionComponent } from './competition/competition.component';
@@ -11,11 +24,33 @@ import { StatsComponent } from './stats/stats.component';
 import { PiscinesComponent } from './piscines/piscines.component';
 import { CotationComponent } from './cotation/cotation.component';
 import { Top10Component } from './top10/top10.component';
+import 'hammerjs';
+
+
+export const routes: Routes = [
+  { path: 'adhesion', component: AdhesionComponent },
+  { path: 'adhesion/:id', component: AdhesionComponent },
+  { path: 'engagement/:idcompet/:idlicencie', component: EngagementComponent },
+
+{ path: '',   component: MainComponent,  children:
+[
+  { path: 'competition', component: CompetitionComponent },
+  { path: 'cotation', component: CotationComponent },
+  { path: 'piscines', component: PiscinesComponent },
+  { path: 'top10', component: Top10Component  },
+  { path: 'stats', component: StatsComponent },
+  { path: 'records', component: RecordsComponent },
+]},
+
+  { path: '**',   component: MainComponent   }
+
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     AdhesionComponent,
     EngagementComponent,
     CompetitionComponent,
@@ -26,7 +61,14 @@ import { Top10Component } from './top10/top10.component';
     Top10Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
