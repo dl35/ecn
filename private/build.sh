beaf2ffb41515b70a6daf895f0962e9cdf04a6ba
lesech35230 <denis.lesech@gmail.com>
2018-02-09 09:23:01 +0100

    rename folder

---
+++ private/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ng build  --output-hashing=bundles --environment=prod --target=production  -base-href=/visucdp/  --output-path=$HOME/workspace2013/svn-web/web-visucdp/meteo/web/ws/visucdp/public
